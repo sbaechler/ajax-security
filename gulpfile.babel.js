9e27b249562e1dbacdb7fa528cdcf36821c25a59
Simon Bächler <simon@stellanera.com>
2016-05-15 17:40:52 +0200

    fix build script

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -46,11 +46,10 @@ gulp.task('lint', lint('app/scripts/**/*.js'));
 gulp.task('lint:test', lint('test/spec/**/*.js', testLintOptions));
 
 gulp.task('html', ['styles', 'scripts'], () => {
-  return gulp.src('app/*.html')
+  return gulp.src(['app/**/*.{html,js,json}'])
     .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
-    .pipe($.if('*.js', $.uglify()))
     .pipe($.if('*.css', $.cssnano()))
-    .pipe($.if('*.html', $.htmlmin({collapseWhitespace: true})))
+    .pipe($.if('*.html', $.htmlmin({collapseWhitespace: false})))
     .pipe(gulp.dest('dist'));
 });
 
